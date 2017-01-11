ed503a65297013b1061d5a77fae15765169cc7b9
German Arcila <germanarcila@gmail.com>
2017-01-10 21:16:02 -0500

    change folders names and html index names

--- js/functions.js
+++ js/functions.js
@@ -1,31 +1,31 @@
 var pContainerHeight = $('.bucket-box').height();
 
-$(window).scroll(function(){
+$(window).scroll(function() {
 
   var wScroll = $(this).scrollTop();
 
   if (wScroll <= pContainerHeight) {
 
     $('.logo').css({
-      'transform' : 'translate(0px, '+ wScroll /2 +'%)'
+      'transform': 'translate(0px, ' + wScroll / 2 + '%)'
     });
 
     $('.bucket').css({
-      'transform' : 'translate('+ wScroll /90 +'%, '+ wScroll /40 +'%)'
+      'transform': 'translate(' + wScroll / 90 + '%, ' + wScroll / 40 + '%)'
     });
 
     $('.pail').css({
-      'transform' : 'translate(-'+wScroll /50 +'%, 0px)'
+      'transform': 'translate(-' + wScroll / 50 + '%, 0px)'
     });
 
   }
 
   // Landing Elements
-  if(wScroll > $('.projects-pics').offset().top - ($(window).height() / 1.2)) {
+  if (wScroll > $('.projects-pics').offset().top - ($(window).height() / 1.2)) {
 
-    $('.projects-pics figure').each(function(i){
+    $('.projects-pics figure').each(function(i) {
 
-      setTimeout(function(){
+      setTimeout(function() {
         $('.projects-pics figure').eq(i).addClass('is-showing');
       }, (700 * (Math.exp(i * 0.14))) - 700);
     });
